f7e078f1ddc017fcbeaa8ab1c9defc2d62c583f7
Charles Kim <javaos74@gmail.com>
2025-04-29 08:27:00 +0900

    Revert "use only required scope"
    This reverts commit a22e309b09107b85ea1afd7be212927857a4e004.

--- Tools/UiPathRobotTool.cs
+++ Tools/UiPathRobotTool.cs
@@ -19,7 +19,7 @@ public class UiPathRobotTool
         var processes = await client.GetProcesses();
         if( processes == null || processes.Count == 0)
         {
-            return "No processes found in specified folders.";
+            return "No processes found.";
         }
         else
         {
@@ -47,7 +47,7 @@ public class UiPathRobotTool
         var release = helper.findProcessWithKey(processKey);
         if( release == null)
         {
-            return "No process found in specified folders.";
+            return "No processes found in specified folder.";
         }
         else        
         {
@@ -69,7 +69,7 @@ public class UiPathRobotTool
         var process = client.GetProcesses().Result.Where( p => p.Key.ToString() == processKey).FirstOrDefault();
         if( process == null)
         {
-            return "No process found in specified folders.";
+            return "No processes found in specified folder.";
         }
         else
         {
